005b7fc6bbca727805710602dbb30738a5101c5d
Vladimir_Artamonov <tomodachiva@gmail.com>
2022-10-02 19:43:35 +0300

    Haker Valiliy Task was done

---
+++ 8_Seminar/Haker_Vasily.py
@@ -0,0 +1,35 @@
+import random as rd
+
+
+def input_data_check():
+    i = True
+    while i:
+        try:
+            input_number = int(
+                input('Введите какое кол-во оценок сгенирировать: '))
+            i = False
+        except ValueError:
+            print('Ввод данных не верен, повторите ввод!')
+    return input_number
+
+
+def get_random_number():
+    rand_num = rd.randint(1, 5)
+    return rand_num
+
+
+numbers = []
+min = 5
+max = 1
+in_num = input_data_check()
+for i in range(0, in_num):
+    number = get_random_number()
+    if min > number:
+        min = number
+    elif max < number:
+        max = number
+    numbers.append(number)
+
+print(f'Вввод: {numbers}\n')
+new_numbers = [min if number == max else number for number in numbers]
+print(f'Выввод: {new_numbers}\n')
